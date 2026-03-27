b1a3b260611c12605dc4819736ba9a3f71ce55ee
Yamini Deore <yamini.deore_it24@pccoer.in>
2026-03-27 23:45:03 +0530

    Create String_reverse.java

---
+++ String_reverse.java
@@ -0,0 +1,29 @@
+// User function Template for Java
+
+class Solution {
+    public static String reverseString(String s) {
+        // code here
+        char[] arr = s.toCharArray();
+        
+        int left = 0 , right = arr.length - 1 ; 
+        
+        while(left < right)
+        {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            
+            left++;
+            right--;
+        }
+        return new String(arr);
+    }
+}  
+
+                                                        or
+class Solution {
+    public static String reverseString(String s) {
+        StringBuilder str = new StringBuilder(s).reverse();
+        return str.toString();        
+    }
+}
